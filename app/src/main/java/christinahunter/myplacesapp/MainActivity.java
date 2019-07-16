7112f59f718b90c575fad1b46f6c14b4a300e17b
christinahunter <frijole16@gmail.com>
2019-07-16 10:49:53 -0700

    Checkpoint 8 Second Commit - action bar add button functional

--- app/src/main/java/christinahunter/myplacesapp/MainActivity.java
+++ app/src/main/java/christinahunter/myplacesapp/MainActivity.java
@@ -1,8 +1,15 @@
 package christinahunter.myplacesapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,8 +17,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PLACE_CREATE = 1;
+
     private ListView mlistView;
     private ArrayAdapter<Place> mArrayAdapter;
+    private ArrayList<Place> myPlaces = new ArrayList<Place>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         mlistView = (ListView) findViewById(R.id.list_view);
 
-        ArrayList<Place> myPlaces = new ArrayList<Place>();
+
         myPlaces.add(new Place("Laney College","Near Lake Merritt"));
         myPlaces.add(new Place("Berkeley City College","Near UC Berkeley"));
         myPlaces.add(new Place("College of Alameda","On Alameda Island"));
@@ -30,4 +40,42 @@ public class MainActivity extends AppCompatActivity {
 
         mlistView.setAdapter(mArrayAdapter);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(resultCode == RESULT_OK && requestCode == REQUEST_PLACE_CREATE){
+            if(data != null){
+                String name = data.getStringExtra("place_name");
+                String description = data.getStringExtra("place_description");
+                mArrayAdapter.add(new Place(name, description));
+
+            }
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.main_menu_create){
+
+            launchCreateActivity();
+
+            return true;
+        }
+        return false;
+    }
+
+    private void launchCreateActivity(){
+
+        Intent createIntent = new Intent(this, PlaceCreateActivity.class);
+        startActivityForResult(createIntent, REQUEST_PLACE_CREATE);
+    }
+
 }
